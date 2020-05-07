856c5f99206443d4a00fd46a164f80b2da943358
Mr.Leon <Mr.Leon@DESKTOP-DFSV9BL>
2020-05-07 13:43:13 +0800

    test

--- src/book_chapter5_string/MaxXorSubarray.java
+++ src/book_chapter5_string/MaxXorSubarray.java
@@ -1,5 +1,38 @@
 package book_chapter5_string;
 
-public class MaxXorSubarray {
+import javax.swing.LayoutFocusTraversalPolicy;
 
+public class MaxXorSubarray {
+	//时间复杂度为O(N^2)
+	public static int maxXorSubarray1(int[] arr) {
+		if(arr == null || arr.length == 0) {
+			return 0;
+		}
+		int[] eor = new int[arr.length];
+		eor[0] = arr[0];
+		for(int i = 1; i < arr.length ; i++) {
+			eor[i] = arr[i] ^ eor[i-1];
+		}
+		int max = Integer.MIN_VALUE;
+		for(int j = 0 ; j < arr.length ;j++) {
+			for(int i = j ; i <= j ; i++) {
+				max = Math.max(max, i == 0 ? eor[j] : eor[j] ^ eor[i -1]);
+			}
+		}
+		return max;
+	}
+	
+}
+class Node{
+	public Node[] nexts = new Node[2];
+}
+class NumTree{
+	public Node head = new Node();
+	public void add(int newNum) {
+		Node cur = head;
+		for(int move = 31 ; move >= 0; move--) {
+			int path = ((newNum >> move) & 1);
+			cur.nexts[path] = cur.nexts[path] == null ? new Node() : cur.nexts[path];
+		}
+	}
 }
--- src/book_chapter5_string/Tire.java
+++ src/book_chapter5_string/Tire.java
@@ -1,5 +1,92 @@
 package book_chapter5_string;
 
-public class Tire {
+import javax.activation.MailcapCommandMap;
 
+class TireNode{
+	public int path;
+	public int end;
+	public TireNode[] map;
+	public TireNode() {
+		path = 0;
+		end = 0;
+		map = new TireNode[26];
+	}
+}
+public class Tire {
+	private TireNode root;
+	public Tire() {
+		 root = new TireNode();
+	}
+	public void insert(String word) {
+		if(word == null) {
+			return;
+		}
+		char[] chas = word.toCharArray();
+		TireNode node = root;
+		node.path++;
+		int index = 0;
+		for(int i = 0 ; i < chas.length;i++) {
+			index = chas[i] - 'a';
+			if(node.map[index] == null) {
+				node.map[index] = new TireNode();
+			}
+			node = node.map[index];
+			node.path++;
+		}
+		node.end++;
+	}
+	public void delete(String word) {
+		if(word == null) {
+			return;
+		}
+		if(search(word)) {
+			char[] chas = word.toCharArray();
+			TireNode node = root;
+			node.path--;
+			int index = 0;
+			for(int i = 0 ; i < chas.length ; i++) {
+				index = chas[i] - 'a';
+				if(node.map[index].path-- == 1) {
+					node.map[index] = null;
+					return;
+				}
+				node = node.map[index];
+			}
+			node.end--;
+		}
+	}
+	public int prefixNumber(String word) {
+		if(word == null) {
+			return 0;
+		}
+		char[] chas = word.toCharArray();
+		TireNode node = root;
+		int index = 0;
+		for(int i = 0 ; i < chas.length; i++) {
+			index = chas[i] - 'a';
+			if(node.map[index] == null) {
+				return 0;
+			}
+			node = node.map[index];
+		}
+		return node.path;
+	}
+	
+	public boolean search(String word) {
+		if(word == null) {
+			return false;
+		}
+		char[] chas = word.toCharArray();
+		TireNode node = root;
+		int index = 0;
+		for(int i = 0 ; i < chas.length; i++) {
+			index = chas[i] - 'a';
+			if(node.map[index] == null) {
+				return false;
+			}
+			node = node.map[index];
+		}
+		
+		return node.end == 0 ? false : true;
+	}
 }
